be8b3375188d91ab3f97c29db6218571d86cd817
chenkunyu66 <48005978+chenkunyu66@users.noreply.github.com>
2019-02-26 01:43:21 -0500

    computer graphic proj01

---
+++ step1/step/resource.h
@@ -0,0 +1,47 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by step.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_stepTYPE                    130
+#define IDD_DIALOG1                     310
+#define IDD_LINEDLG                     310
+#define IDD_DIALOG2                     312
+#define IDD_DIALOG3                     314
+#define IDD_STARINPUT                   316
+#define IDD_DIALOG4                     318
+#define IDD_STARDLG                     318
+#define IDC_EDIT1                       1000
+#define IDC_FMY                         1000
+#define IDC_STAR                        1000
+#define IDC_EDIT2                       1001
+#define IDC_TOX                         1001
+#define IDC_STAR2                       1001
+#define IDC_Y                           1001
+#define IDC_EDIT3                       1002
+#define IDC_TOY                         1002
+#define IDC_STAR3                       1002
+#define IDC_LENGTH                      1002
+#define IDC_EDIT4                       1003
+#define IDC_FMX                         1003
+#define IDC_STAR4                       1003
+#define IDC_ANGLE                       1003
+#define IDC_X                           1004
+#define ID_STEPSTUFF_LINEENDTO0         32771
+#define ID_STEPSTUFF_LINEDIALOG         32772
+#define ID_STEPSTUFF_STARINPUT          32773
+#define ID_STEPSTUFF_ROTATE25           32774
+#define ID_STEPSTUFF_STARDIALOG         32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        320
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
